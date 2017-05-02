19ba48835d30c76d2eab335038dd09b3761dc523
aisleypay <paisley.singh@gmail.com>
2017-05-02 16:49:09 -0400

    Done.

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,40 +1,59 @@
 require 'rails_helper'
 
-RSpec.describe StudentsController, :type => :controller do
+describe StudentsController do
 
-  describe "GET new" do
-    it "returns http success" do
-      get :new
-      expect(response).to be_success
-    end
-  end
+ describe "POST create" do
+   context "with valid attributes" do
+     it "creates a new student" do
+       expect{
+         post :create, { :student => { :first_name => "Bill", :last_name => "Smith" } }
+       }.to change(Student,:count).by(1)
+     end
 
-  describe "GET create" do
-    it "returns http success" do
-      get :create
-      expect(response).to be_success
-    end
-  end
-
-  describe "GET show" do
-    it "returns http success" do
-      get :show, id: 1
-      expect(response).to be_success
-    end
-  end
-
-  describe "GET edit" do
-    it "returns http success" do
-      get :edit, id: 1
-      expect(response).to be_success
-    end
-  end
-
-  describe "GET update" do
-    it "returns http success" do
-      get :update, id: 1
-      expect(response).to be_success
-    end
-  end
+     it "redirects to the new student" do
+       post :create, { :student => { :first_name => "Bill", :last_name => "Smith" } }
+       expect(response).to redirect_to Student.last
+     end
+   end
+ end
 
 end
+
+# RSpec.describe StudentsController, :type => :controller do
+#
+#   describe "GET new" do
+#     it "returns http success" do
+#       get :new
+#       expect(response).to be_success
+#     end
+#   end
+#
+#   describe "GET create" do
+#     it "returns http success" do
+#       get :create
+#       expect(response).to be_success
+#     end
+#   end
+#
+#   describe "GET show" do
+#     it "returns http success" do
+#       get :show, id: 1
+#       expect(response).to be_success
+#     end
+#   end
+#
+#   describe "GET edit" do
+#     it "returns http success" do
+#       get :edit, id: 1
+#       expect(response).to be_success
+#     end
+#   end
+#
+#   describe "GET update" do
+#     it "returns http success" do
+#       get :update, id: 1
+#       expect(response).to be_success
+#     end
+#   end
+#
+# end
